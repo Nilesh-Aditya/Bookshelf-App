148a18d3ebdbc5c0d8640a9d205b3801912fee1e
Nilesh Aditya <nileshaditya2000@gmail.com>
2021-12-09 04:52:29 +0530

    deployment

--- server.js
+++ server.js
@@ -6,24 +6,20 @@ const path = require('path');
 const app = express();
 connectDB();
 
-app.use(express.static('client/public'));
+// app.use(express.static(path.join(__dirname)));
 app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
 
 app.use('/api', require('./routes/api'));
 
-app.listen(PORT, () => console.log(`server is running on http://localhost:${PORT}`));
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+    
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
-
-app.get('/', (req, res) => {
-    res.send( '<p>Hello, Welcome to the Server</p>' );
-})
+app.listen(PORT, () => console.log(`server is running on http://localhost:${PORT}`));

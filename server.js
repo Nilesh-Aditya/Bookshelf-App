6f706491dada139f58296213989e4013b5cbca43
Nilesh Aditya <nileshaditya2000@gmail.com>
2021-12-09 04:25:31 +0530

    v1

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const connectDB = require('./data/db');
+const path = require('path');
 
 const app = express();
 connectDB();
@@ -10,11 +11,19 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
 
+app.use('/api', require('./routes/api'));
+
 app.listen(PORT, () => console.log(`server is running on http://localhost:${PORT}`));
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 
 app.get('/', (req, res) => {
     res.send( '<p>Hello, Welcome to the Server</p>' );
 })
-
-app.use('/api', require('./routes/api'));
